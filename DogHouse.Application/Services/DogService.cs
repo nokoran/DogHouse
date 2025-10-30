6e219716faf29ab711d9af74fce31180b7a1e1db
Mykyta Ropalo <ser.nik.ropalo@gmail.com>
2025-10-30 14:26:16 +0200

    Implemented DogService

--- DogHouse.Application/Services/DogService.cs
+++ DogHouse.Application/Services/DogService.cs
@@ -1,16 +1,41 @@
 ﻿using DogHouse.Api.DTOs;
+using DogHouse.Application.Interfaces;
+using DogHouse.Domain.Entities;
 
 namespace DogHouse.Application.Services;
 
 public class DogService : IDogService
 {
+    private readonly IDogRepository _dogRepository;
+    public DogService(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
     public async Task<IEnumerable<DogResponse>> GetDogsAsync(PaginationQuery query)
     {
-        throw new NotImplementedException();
+        var dogs = await _dogRepository.GetDogsAsync(query.Attribute, query.Order, query.PageNumber, query.PageSize);
+        return dogs.Select(dog => new DogResponse
+        {
+            Name = dog.Name,
+            Color = dog.Color,
+            TailLength = dog.TailLength,
+            Weight = dog.Weight
+        });
     }
 
     public async Task CreateDogAsync(CreateDogRequest request)
     {
-        throw new NotImplementedException();
+        if (await _dogRepository.DoesDogNameExistAsync(request.Name))
+        {
+            throw new InvalidOperationException("Dog with this name already exists.");
+        }
+        Dog newDog = new Dog
+        {
+            Name = request.Name,
+            Color = request.Color,
+            TailLength = request.TailLength,
+            Weight = request.Weight
+        };
+        await _dogRepository.AddDogAsync(newDog);
     }
 }
