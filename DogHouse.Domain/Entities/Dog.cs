fb6a4d98efb0a1a4b3ab62b62c6f643e4c2027a9
Mykyta Ropalo <ser.nik.ropalo@gmail.com>
2025-10-30 12:07:33 +0200

    Add properties to Dog class for entity framework mapping

--- DogHouse.Domain/Entities/Dog.cs
+++ DogHouse.Domain/Entities/Dog.cs
@@ -1,6 +1,12 @@
 ﻿namespace DogHouse.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class Dog
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int TailLength { get; set; }
+    public int Weight { get; set; }
 }
